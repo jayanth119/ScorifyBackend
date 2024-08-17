7e57d776ad3ce421fcc53d6f3c8d8719c07e5001
Raju1422 <111197741+Raju1422@users.noreply.github.com>
2024-08-17 13:38:56 +0530

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('core.urls')),
     path('' , include('epc.urls')),
-    path('',include('regularmaintaince.urls'))
+    # path('',include('regularmaintaince.urls'))
 ]
