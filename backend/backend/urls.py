eab0e24db69407b1452ad7007e78b5dd096de5c7
jayanth119 <chjayanth119@gmail.com>
2024-08-20 15:48:20 +0530

    add post man api

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -24,5 +24,7 @@ urlpatterns = [
     path('' , include("gasandheat.urls")),
     path('accounts/',include('authentication.urls')), 
     path('',include('regularmaintaince.urls')),
-    path('',include('inventory_insception.urls'))
+    path('',include('inventory_insception.urls')) , 
+    path('' , include('mould.urls'))
+    
  ]
